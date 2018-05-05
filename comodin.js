ab3fafd4fc8ae6686c08a4684ab4b6b95eb75798
agg1357 <33790805+agg1357@users.noreply.github.com>
2018-05-05 18:43:09 +0200

    Update comodin.js

--- comodin.js
+++ comodin.js
@@ -1,4 +1,17 @@
 
+//PARA LIBROS
+//show_banner="0" NO MOSTRAR BANNER   show_banner="1" MOSTRAR BANNER
+show_banner=1
+
+//PARA LIBROS
+//show_interstitial1  A PARTIR DE CUANDO SE MUENSTRA INTERSTITAL
+//show_interstitial2  CADA CUANDO DESPUÉS DEL PRIMERO SE MUESTRA INTERSTITIAL
+//show_interstitialtiempo TIEMPO MÍNIMO PARA MOSTRAR INTERSTITAL en seg.
+show_interstitial1=12 
+show_interstitial2=10
+show_interstitialtiempo=100
+
+
 
 comodinx=7
 comodiny=14
