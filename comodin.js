9277ceb4b5400a440c5b46fa0e5be6c46543637f
agg1357 <33790805+agg1357@users.noreply.github.com>
2018-05-05 21:26:09 +0200

    Update comodin.js

--- comodin.js
+++ comodin.js
@@ -1,15 +1,21 @@
 
 //PARA LIBROS
-//show_banner="0" NO MOSTRAR BANNER   show_banner="1" MOSTRAR BANNER
+//show_banner=0 NO MOSTRAR BANNER   show_banner=1 MOSTRAR BANNER
 show_banner=1
 
 //PARA LIBROS
-//show_interstitial1  A PARTIR DE CUANDO SE MUENSTRA INTERSTITAL
-//show_interstitial2  CADA CUANDO DESPUÉS DEL PRIMERO SE MUESTRA INTERSTITIAL
-//show_interstitialtiempo TIEMPO MÍNIMO PARA QUE PUEDA APARECER SEGUNDO Y RESTANTES INTERSTITIALES
-show_interstitial1=12 
-show_interstitial2=10
-show_interstitialtiempo=100
+//show_interstitial1  CONTADOR A PARTIR DE CUANDO SE MUESTRA PRIMER INTERSTITIAL. 
+            //Example. show_interstitial1=10.  El 1º Interstitial saldrá a la 10 pulsación (si show_interstitialtiempo1 no lo impide) 
+//show_interstitial2  CONTADOR CADA CUANDO SE MUESTRAN SEGUNDO Y RESTO INTERSTITIALS
+            //Example. show_interstitial2=12.  El resto Interstitials saldrá a la 12 pulsación después de 1º Interstitial (si show_interstitialtiempo2 no lo impide) 
+//show_interstitialtiempo1 TIEMPO MÍNIMO PARA QUE PUEDA APARECER PRIMER INTERSTITIAL (seg.)
+            //Example.  show_interstitialtiempo1=20 (precisión cada 10 seg) por lo que se habilita 1º interstitial a los 30 seg.
+//show_interstitialtiempo2 TIEMPO MÍNIMO PARA QUE PUEDA APARECER SEGUNDO Y RESTO INTERSTITIALS (seg)
+            //Example.  show_interstitialtiempo1=30 (precisión cada 10 seg) por lo que se habilita resto interstitials a los 40 seg. después de 1º interstitial
+show_interstitial1=10 
+show_interstitial2=5
+show_interstitialtiempo1=20
+show_interstitialtiempo1=10
 
 
 
