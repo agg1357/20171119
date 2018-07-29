027d8f79b7bf94dca1a60c94190ec61fc6ac7a4c
agg1357 <33790805+agg1357@users.noreply.github.com>
2018-07-29 12:00:49 +0200

    Update comodin.js

--- comodin.js
+++ comodin.js
@@ -28,8 +28,8 @@ show_interstitialtiempo2_verb=-11
 
 
 //para MAGIC 2 LIBRE
-//show_spell2=0 ACTÚAN LOS VALORES INFERIORES. show_spell2=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR RECETA HECHIZO
 show_spell2=0
+//show_spell2=0 ACTÚAN LOS VALORES INFERIORES. show_spell2=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR RECETA HECHIZO
 show_banner_spell2=1
 show_interstitial1_spell2=3 
 show_interstitial2_spell2=10
