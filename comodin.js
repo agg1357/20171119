9eee0beeefd692251907a02193ad89efdc8dab8b
agg1357 <33790805+agg1357@users.noreply.github.com>
2019-07-17 21:40:58 +0200

    Update comodin.js

--- comodin.js
+++ comodin.js
@@ -17,6 +17,27 @@ show_interstitialtiempo2=10
 
 
 
+//PARA Pride Oculto
+//show_banner=0 NO MOSTRAR BANNER   show_banner=1 MOSTRAR BANNER
+//show_interstitial1  CONTADOR A PARTIR DE CUANDO SE MUESTRA PRIMER INTERSTITIAL. 
+            //Example. show_interstitial1=10.  El 1º Interstitial saldrá a la 11 pulsación (si show_interstitialtiempo1 no lo impide) 
+//show_interstitial2  CONTADOR CADA CUANDO SE MUESTRAN SEGUNDO Y RESTO INTERSTITIALS
+            //Example. show_interstitial2=12.  El resto Interstitials saldrá a la 13 pulsación después de 1º Interstitial (si show_interstitialtiempo2 no lo impide) 
+//show_interstitialtiempo1 TIEMPO MÍNIMO PARA QUE PUEDA APARECER PRIMER INTERSTITIAL (seg.)
+            //Example.  show_interstitialtiempo1=20 (precisión cada 10 seg) por lo que se habilita 1º interstitial a los 30 seg.
+//show_interstitialtiempo2 TIEMPO MÍNIMO PARA QUE PUEDA APARECER SEGUNDO Y RESTO INTERSTITIALS (seg)
+            //Example.  show_interstitialtiempo1=30 (precisión cada 10 seg) por lo que se habilita resto interstitials a los 40 seg. después de 1º interstitial
+show_bannerPride=1
+show_interstitial1Pride=2 
+show_interstitial2Pride=8
+show_interstitialtiempo1Pride=10
+show_interstitialtiempo2Pride=10
+
+
+
+
+
+
 //para VERB TRAINER
 show_banner_verb=1
 show_interstitial1_verb=0 
