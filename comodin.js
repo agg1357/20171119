46fc9fc3e99e5785e4e55a14a7540534f85ecf9a
agg1357 <33790805+agg1357@users.noreply.github.com>
2020-11-21 20:50:09 +0100

    Update comodin.js

--- comodin.js
+++ comodin.js
@@ -10,7 +10,7 @@
 //show_interstitialtiempo2 TIEMPO MÍNIMO PARA QUE PUEDA APARECER SEGUNDO Y RESTO INTERSTITIALS (seg)
             //Example.  show_interstitialtiempo1=30 (precisión cada 10 seg) por lo que se habilita resto interstitials a los 40 seg. después de 1º interstitial
 show_banner=1
-show_interstitial1=2 
+show_interstitial1=2000 
 show_interstitial2=8
 show_interstitialtiempo1=10
 show_interstitialtiempo2=10
@@ -35,7 +35,7 @@ show_interstitialtiempo2Pride=10
 
 //PARA Sense Oculto
 show_bannerSense=1
-show_interstitial1Sense=3 
+show_interstitial1Sense=3000 
 show_interstitial2Sense=8
 show_interstitialtiempo1Sense=10
 show_interstitialtiempo2Sense=10
@@ -52,7 +52,7 @@ show_interstitialtiempo2War=10
 
 //PARA Montecristo Oculto
 show_bannerMonte=1
-show_interstitial1Monte=3 
+show_interstitial1Monte=3000 
 show_interstitial2Monte=8
 show_interstitialtiempo1Monte=10
 show_interstitialtiempo2Monte=10
@@ -60,7 +60,7 @@ show_interstitialtiempo2Monte=10
 
 //PARA Wuthering Oculto
 show_bannerWuthering=1
-show_interstitial1Wuthering=3 
+show_interstitial1Wuthering=3000 
 show_interstitial2Wuthering=3
 show_interstitialtiempo1Wuthering=10
 show_interstitialtiempo2Wuthering=10
@@ -68,7 +68,7 @@ show_interstitialtiempo2Wuthering=10
 
 //PARA Jane Eyre Oculto
 show_bannerEyre=1
-show_interstitial1Eyre=3 
+show_interstitial1Eyre=3000 
 show_interstitial2Eyre=8
 show_interstitialtiempo1Eyre=10
 show_interstitialtiempo2Eyre=10
@@ -121,28 +121,6 @@ show_interstitialtiempo2_runa=300
 promo_powerful=0
 
 
-//para ULTIMATE LIBRE
-show_ultimate=1
-//show_ultimate=0 ACTÚAN LOS VALORES INFERIORES. show_ultimate=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR RECETA HECHIZO
-//promo_powerfulU=0 no hay autopromocion de mis apps. promo_powerfulU=1 habilito autopromocion
-show_banner_ultimate=1
-show_interstitial1_ultimate=1 
-show_interstitial2_ultimate=1
-show_interstitialtiempo1_ultimate=100
-show_interstitialtiempo2_ultimate=100
-promo_powerfulU=1
-
-
-//para DEMONS
-show_demons=1
-//show_frases=0 ACTÚAN LOS VALORES INFERIORES. show_frases=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR capitulos
-//promo_powerfulF=0 no hay autopromocion de mis apps. promo_powerfulF=1 habilito autopromocion
-show_banner_demons=1
-show_interstitial1_demons=1 
-show_interstitial2_demons=1
-show_interstitialtiempo1_demons=100
-show_interstitialtiempo2_demons=200
-
 
 //para 3M3 SPELLS
 show_3M3=1
@@ -164,18 +142,6 @@ show_interstitialtiempo2_Black_Magic=30
 
 
 
-//para FRASES
-show_frases=1
-//show_frases=0 ACTÚAN LOS VALORES INFERIORES. show_frases=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR capitulos
-//promo_powerfulF=0 no hay autopromocion de mis apps. promo_powerfulF=1 habilito autopromocion
-show_banner_frases=1
-show_interstitial1_frases=1 
-show_interstitial2_frases=1
-show_interstitialtiempo1_frases=10
-show_interstitialtiempo2_frases=10
-promo_powerfulF=1
-
-
 
 //para HOROSCOPO MÁGICO LIBRE
 show_horo=1
@@ -186,45 +152,6 @@ show_interstitial2_horo=1
 show_interstitialtiempo1_horo=-11
 show_interstitialtiempo2_horo=10
 
-//para ARCHANGEL
-show_archangel=1
-//show_archangel=0 ACTÚAN LOS VALORES INFERIORES. show_archangel=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR capitulos
-//promo_powerfulA=0 no hay autopromocion de mis apps. promo_powerfulA=1 habilito autopromocion
-show_banner_archangel=1
-show_interstitial1_archangel=1 
-show_interstitial2_archangel=1
-show_interstitialtiempo1_archangel=20
-show_interstitialtiempo2_archangel=20
-promo_powerfulF=0
-
-
-//para CHINESE ZODIAC
-show_chino=1
-//show_archangel=0 ACTÚAN LOS VALORES INFERIORES. show_archangel=1 ACTÚAN LOS VALORES INFERIORES Y ADEMÁS INTERSTITIAL AL MOSTRAR capitulos
-//promo_powerfulA=0 no hay autopromocion de mis apps. promo_powerfulA=1 habilito autopromocion
-show_banner_chino=1
-show_interstitial1_chino=1 
-show_interstitial2_chino=1
-show_interstitialtiempo1_chino=20
-show_interstitialtiempo2_chino=20
-
-
-//para Prayers of thanks to God
-show_PrayerThanks=1
-show_banner_PrayerThanks=1
-show_interstitial1_PrayerThanks=1000 
-show_interstitial2_PrayerThanks=1000
-show_interstitialtiempo1_PrayerThanks=2
-show_interstitialtiempo2_PrayerThanks=2
-
-
-//para Greek Gods
-show_greekGods=1
-show_banner_greekGods=1
-show_interstitial1_greekGods=1000 
-show_interstitial2_greekGods=1000
-show_interstitialtiempo1_greekGods=2
-show_interstitialtiempo2_greekGods=2
 
 
 
